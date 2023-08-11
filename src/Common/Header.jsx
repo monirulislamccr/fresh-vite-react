8f2a2d45658f4e801851ad62341be9969cc5e546
A S M Monirul Islam <monirulislamccr@gmail.com>
2023-08-12 00:26:46 +0600

    configuration added

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div className='text-center text-5xl my-9'>
+        <div className='text-center text-5xl my-9 text-black'>
             This is header
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-      extend: {},
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '4rem',
+                xl: '5rem',
+                '2xl': '6rem',
+              },
+        },
+        colors: {
+            transparent: 'transparent',
+            black: 'red',
+            white: '#fff',
+            gray: {
+              100: '#f7fafc',
+              // ...
+              900: '#1a202c',
+            },
+            current: 'currentColor',
+            'purple': '#3f3cbb',
+            'midnight': '#121063',
+            'metal': '#565584',
+            'tahiti': '#3ab7bf',
+            'silver': '#ecebff',
+            'bubble-gum': '#ff77e9',
+            'bermuda': '#78dcca',
+            // ...
+        },
+        screens: {
+            //https://tailwindcss.com/docs/screens#adding-larger-breakpoints
+          },
+        extend: {},
     },
     daisyui: {
         themes: ["light"],
-      },
-    plugins: [require("daisyui")]
-  }
+    },
+    plugins: [require("daisyui")],
+};
